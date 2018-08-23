11acc8cf7ccf49701a1a795af7048e28104e284f
Ben Yanke <byanke@pcnametag.com>
2018-08-23 10:01:48 -0500

    Update env var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo ""
 echo "Configuring events based on your env vars"
 
 sed -i 's/EVENT_PORT/'"$EVENT_PORT"'/g' /etc/taiga-events/config.json
-sed -i 's/EVENT_SECRET/'"$EVENT_SECRET"'/g' /etc/taiga-events/config.json
+sed -i 's/EVENT_SECRET/'"$TAIGA_SECRET_KEY"'/g' /etc/taiga-events/config.json
 sed -i 's/EVENT_USER/'"$EVENT_USER"'/g' /etc/taiga-events/config.json
 sed -i 's/EVENT_PW/'"$EVENT_PW"'/g' /etc/taiga-events/config.json
 sed -i 's/EVENT_HOST/'"$EVENT_HOST"'/g' /etc/taiga-events/config.json
